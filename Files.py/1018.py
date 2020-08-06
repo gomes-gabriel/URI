6c2f6b2d08325e22f09b91e7172fa7e8fea5da40
Gabriel Gomes <gabrielsantosgomes9@gmail.com>
2020-08-06 12:19:27 -0300

    Update 1018.py

--- Files.py/1018.py
+++ Files.py/1018.py
@@ -1,21 +1,13 @@
-import locale
-
-# Mudando a localização para o padrão brasileiro
-locale.setlocale(locale.LC_ALL, 'pt-BR')
-
 # Recebendo o valor de entrada
 N = int(input())
 
 # Tuple com os valores de notas padrão
-notas = (100, 50, 20, 10, 5, 2, 1)
+notas = (100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00)
 
 # Printando o valor de entrada
 print(N)
 
 for nota in notas:
-    # Modificando a nota para o padrão brasileiro com R$
-    nota_formatada = locale.currency(nota)
-
     # Printando a saída formatada para a quantidade de cada nota
-    print('%d nota(s) de' % (N/nota), nota_formatada)
+    print('%d nota(s) de' % (N/nota), 'R$ %s' % (str(nota).replace('.', ',0')))
     N = N % nota
