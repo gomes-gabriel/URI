7750b9debc942508b9da1adff716d1c1e9d91f0a
Gabriel Gomes <gabrielsantosgomes9@gmail.com>
2020-08-10 13:32:33 -0300

    Create 1035.py

---
+++ Files.py/1035.py
@@ -0,0 +1,8 @@
+inp = list(map(int, input().split(' ')))
+
+A, B, C, D = inp
+
+if B > C and D > A and C + D > A + B and C > 0 and D > 0 and A % 2 == 0:
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
